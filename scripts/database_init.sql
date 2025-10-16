2c045994a55239d59f94d9cb9eee3e1455085f1c
Immaculate Okoro <ermmaculate97@gmail.com>
2025-10-16 09:53:25 +0100

    Update database_init.sql

--- scripts/database_init.sql
+++ scripts/database_init.sql
@@ -1,4 +1,10 @@
 --initializing database------
+/*
+This script creates a new database named "market_sales" after checking if it already exists.
+If exists, it is replaced . Additionally, the script sets up a schema "gold" and a staging area "gold_stage"
+
+Warning: Running this script will replace the entire 'market_sales' database if it already exists, and all records will be permanently lost. Therefore proceed with caution
+*/
 
 CREATE OR REPLACE DATABASE market_sales;
  
